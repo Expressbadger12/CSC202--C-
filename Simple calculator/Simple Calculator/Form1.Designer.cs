6e73838b279daf0b890c3836ab40fe5e184c2308
Boone <68450612+Expressbadger12@users.noreply.github.com>
2025-01-28 14:53:35 -0700

    Add files via upload

---
+++ Simple calculator/Simple Calculator/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Simple_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            FirstNumber_Textbox = new TextBox();
+            SecondNumber_Textbox = new TextBox();
+            Result_Textbox = new TextBox();
+            Calculate_Button = new Button();
+            Close_Button = new Button();
+            multiplybutton = new Button();
+            subtract = new Button();
+            dividebutton = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(76, 44);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Enter First Number";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(57, 96);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(123, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Enter Second Number";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(146, 151);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Result";
+            label3.Click += label3_Click;
+            // 
+            // FirstNumber_Textbox
+            // 
+            FirstNumber_Textbox.Location = new Point(233, 44);
+            FirstNumber_Textbox.Margin = new Padding(2);
+            FirstNumber_Textbox.Name = "FirstNumber_Textbox";
+            FirstNumber_Textbox.Size = new Size(106, 23);
+            FirstNumber_Textbox.TabIndex = 3;
+            // 
+            // SecondNumber_Textbox
+            // 
+            SecondNumber_Textbox.Location = new Point(233, 96);
+            SecondNumber_Textbox.Margin = new Padding(2);
+            SecondNumber_Textbox.Name = "SecondNumber_Textbox";
+            SecondNumber_Textbox.Size = new Size(106, 23);
+            SecondNumber_Textbox.TabIndex = 4;
+            // 
+            // Result_Textbox
+            // 
+            Result_Textbox.Location = new Point(233, 151);
+            Result_Textbox.Margin = new Padding(2);
+            Result_Textbox.Name = "Result_Textbox";
+            Result_Textbox.ReadOnly = true;
+            Result_Textbox.Size = new Size(106, 23);
+            Result_Textbox.TabIndex = 5;
+            // 
+            // Calculate_Button
+            // 
+            Calculate_Button.Location = new Point(107, 127);
+            Calculate_Button.Margin = new Padding(2);
+            Calculate_Button.Name = "Calculate_Button";
+            Calculate_Button.Size = new Size(78, 20);
+            Calculate_Button.TabIndex = 6;
+            Calculate_Button.Text = "Add";
+            Calculate_Button.UseVisualStyleBackColor = true;
+            Calculate_Button.Click += Calculate_Button_Click;
+            // 
+            // Close_Button
+            // 
+            Close_Button.Location = new Point(245, 209);
+            Close_Button.Margin = new Padding(2);
+            Close_Button.Name = "Close_Button";
+            Close_Button.Size = new Size(78, 20);
+            Close_Button.TabIndex = 7;
+            Close_Button.Text = "Close";
+            Close_Button.UseVisualStyleBackColor = true;
+            Close_Button.Click += Close_Button_Click;
+            // 
+            // multiplybutton
+            // 
+            multiplybutton.Location = new Point(281, 127);
+            multiplybutton.Margin = new Padding(2);
+            multiplybutton.Name = "multiplybutton";
+            multiplybutton.Size = new Size(78, 20);
+            multiplybutton.TabIndex = 8;
+            multiplybutton.Text = "Multiply";
+            multiplybutton.UseVisualStyleBackColor = true;
+            multiplybutton.Click += multiplybutton_Click;
+            // 
+            // subtract
+            // 
+            subtract.Location = new Point(199, 127);
+            subtract.Margin = new Padding(2);
+            subtract.Name = "subtract";
+            subtract.Size = new Size(78, 20);
+            subtract.TabIndex = 9;
+            subtract.Text = "Subtract";
+            subtract.UseVisualStyleBackColor = true;
+            subtract.Click += subtract_Click;
+            // 
+            // dividebutton
+            // 
+            dividebutton.Location = new Point(372, 127);
+            dividebutton.Margin = new Padding(2);
+            dividebutton.Name = "dividebutton";
+            dividebutton.Size = new Size(78, 20);
+            dividebutton.TabIndex = 10;
+            dividebutton.Text = "divide";
+            dividebutton.UseVisualStyleBackColor = true;
+            dividebutton.Click += dividebutton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(560, 270);
+            Controls.Add(dividebutton);
+            Controls.Add(subtract);
+            Controls.Add(multiplybutton);
+            Controls.Add(Close_Button);
+            Controls.Add(Calculate_Button);
+            Controls.Add(Result_Textbox);
+            Controls.Add(SecondNumber_Textbox);
+            Controls.Add(FirstNumber_Textbox);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Margin = new Padding(2);
+            Name = "Form1";
+            Text = "Simple Calculator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox FirstNumber_Textbox;
+        private TextBox SecondNumber_Textbox;
+        private TextBox Result_Textbox;
+        private Button Calculate_Button;
+        private Button Close_Button;
+        private Button multiplybutton;
+        private Button subtract;
+        private Button dividebutton;
+    }
+}
---
+++ csc202guifrontend/csc202guifrontend/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace csc202guifrontend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FSLabel = new System.Windows.Forms.Label();
+            this.lslabel = new System.Windows.Forms.Label();
+            this.FNBox = new System.Windows.Forms.TextBox();
+            this.LNbox = new System.Windows.Forms.TextBox();
+            this.emaillabel = new System.Windows.Forms.Label();
+            this.emailbox = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.submitbutton = new System.Windows.Forms.Button();
+            this.closebutton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.rolelabel = new System.Windows.Forms.Label();
+            this.Agelabel = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FSLabel
+            // 
+            this.FSLabel.AutoSize = true;
+            this.FSLabel.Location = new System.Drawing.Point(220, 93);
+            this.FSLabel.Name = "FSLabel";
+            this.FSLabel.Size = new System.Drawing.Size(58, 14);
+            this.FSLabel.TabIndex = 0;
+            this.FSLabel.Text = "First Name";
+            // 
+            // lslabel
+            // 
+            this.lslabel.AutoSize = true;
+            this.lslabel.Location = new System.Drawing.Point(220, 142);
+            this.lslabel.Name = "lslabel";
+            this.lslabel.Size = new System.Drawing.Size(57, 14);
+            this.lslabel.TabIndex = 1;
+            this.lslabel.Text = "Last Name";
+            // 
+            // FNBox
+            // 
+            this.FNBox.Location = new System.Drawing.Point(343, 93);
+            this.FNBox.Name = "FNBox";
+            this.FNBox.Size = new System.Drawing.Size(100, 20);
+            this.FNBox.TabIndex = 2;
+            // 
+            // LNbox
+            // 
+            this.LNbox.Location = new System.Drawing.Point(343, 142);
+            this.LNbox.Name = "LNbox";
+            this.LNbox.Size = new System.Drawing.Size(100, 20);
+            this.LNbox.TabIndex = 3;
+            // 
+            // emaillabel
+            // 
+            this.emaillabel.AutoSize = true;
+            this.emaillabel.Location = new System.Drawing.Point(223, 201);
+            this.emaillabel.Name = "emaillabel";
+            this.emaillabel.Size = new System.Drawing.Size(34, 14);
+            this.emaillabel.TabIndex = 4;
+            this.emaillabel.Text = "Email";
+            this.emaillabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // emailbox
+            // 
+            this.emailbox.Location = new System.Drawing.Point(343, 194);
+            this.emailbox.Name = "emailbox";
+            this.emailbox.Size = new System.Drawing.Size(100, 20);
+            this.emailbox.TabIndex = 5;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(460, 197);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(217, 18);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "I would like to receive email notifications";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // submitbutton
+            // 
+            this.submitbutton.Location = new System.Drawing.Point(223, 374);
+            this.submitbutton.Name = "submitbutton";
+            this.submitbutton.Size = new System.Drawing.Size(75, 25);
+            this.submitbutton.TabIndex = 7;
+            this.submitbutton.Text = "Submit";
+            this.submitbutton.UseVisualStyleBackColor = true;
+            // 
+            // closebutton
+            // 
+            this.closebutton.Location = new System.Drawing.Point(460, 374);
+            this.closebutton.Name = "closebutton";
+            this.closebutton.Size = new System.Drawing.Size(75, 25);
+            this.closebutton.TabIndex = 8;
+            this.closebutton.Text = "Close";
+            this.closebutton.UseVisualStyleBackColor = true;
+            this.closebutton.Click += new System.EventHandler(this.closebutton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Student",
+            "Teacher",
+            "Clown"});
+            this.comboBox1.Location = new System.Drawing.Point(343, 292);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 22);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // rolelabel
+            // 
+            this.rolelabel.AutoSize = true;
+            this.rolelabel.Location = new System.Drawing.Point(223, 299);
+            this.rolelabel.Name = "rolelabel";
+            this.rolelabel.Size = new System.Drawing.Size(28, 14);
+            this.rolelabel.TabIndex = 10;
+            this.rolelabel.Text = "Role";
+            // 
+            // Agelabel
+            // 
+            this.Agelabel.AutoSize = true;
+            this.Agelabel.Location = new System.Drawing.Point(223, 252);
+            this.Agelabel.Name = "Agelabel";
+            this.Agelabel.Size = new System.Drawing.Size(25, 14);
+            this.Agelabel.TabIndex = 11;
+            this.Agelabel.Text = "Age";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(343, 252);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 485);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.Agelabel);
+            this.Controls.Add(this.rolelabel);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.closebutton);
+            this.Controls.Add(this.submitbutton);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.emailbox);
+            this.Controls.Add(this.emaillabel);
+            this.Controls.Add(this.LNbox);
+            this.Controls.Add(this.FNBox);
+            this.Controls.Add(this.lslabel);
+            this.Controls.Add(this.FSLabel);
+            this.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Sign Up";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FSLabel;
+        private System.Windows.Forms.Label lslabel;
+        private System.Windows.Forms.TextBox FNBox;
+        private System.Windows.Forms.TextBox LNbox;
+        private System.Windows.Forms.Label emaillabel;
+        private System.Windows.Forms.TextBox emailbox;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button submitbutton;
+        private System.Windows.Forms.Button closebutton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label rolelabel;
+        private System.Windows.Forms.Label Agelabel;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
